d1c3c4b0f977bfe542efb8019f4de345e02d4c31
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-13 16:42:34 +0000

    [add README Gem model controller view routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root :to => "contents#index"
+    
+    
+    resource :homes do
+     get :about, on: :collection
+    end
+    
+
+    resources :users, only: [:index, :show, :edit, :update] do
+     resource :relationships, only: [:index, :create, :destroy]
+    end
+    
+
+    resources :contents do
+     resource :favorites, only: [:index, :create, :destroy]
+     resources :comments, only: [:index, :create, :destroy]
+    end
+    
+    
+    get 'chat/:id' => 'chats#show', as: 'chat'
+ 
+    resources :chats, only: [:index, :create]
+  
+  
 end
