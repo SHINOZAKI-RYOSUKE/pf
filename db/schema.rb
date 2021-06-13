f54a31cb0809ec571d2ea87634f8b4a63599ecd7
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-13 17:43:41 +0000

    [add users/index]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_161223) do
+ActiveRecord::Schema.define(version: 2021_06_13_174017) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_161223) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.string "profile_image"
+    t.string "profile_image_id"
     t.text "introduction"
     t.string "greeting"
     t.datetime "created_at", null: false
