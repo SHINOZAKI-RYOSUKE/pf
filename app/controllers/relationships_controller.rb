b13c2ed8922954cbd92ff6efe08f8e3a37a3949a
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-16 06:37:08 +0000

    [fix controller/rilationships add db/seed.rb]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,20 +1,20 @@
 class RelationshipsController < ApplicationController
   def index
     user = User.find(params[:user_id])
-    @user_A = user.followings
-    @user_P = user.follower
+    @users_A = user.followings
+    @users_P = user.followers
   end
 
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_back(fallback_location: root_path)
+    redirect_to request.referer
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to request.referer
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,12 @@ Rails.application.routes.draw do
     
 
     resources :users, only: [:index, :show, :edit, :update] do
-     resource :relationships, only: [:index, :create, :destroy]
+     resources :relationships, only: [:index, :create, :destroy]
     end
     
 
     resources :contents do
-     resource :favorites, only: [:index, :create, :destroy]
+     resources :favorites, only: [:index, :create, :destroy]
      resources :comments, only: [:index, :create, :destroy]
     end
     
