cae416c7c0a28c4abf5cbfc5910e51813916bbce
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-17 11:01:45 +0000

    [add controller/comment(index create destroy) view/comment(_c_comment creat.ja.erb destroy.js.erb)]

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,36 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @comments = Comment.all
+    
+    @content = Content.find(params[:content_id])
+    @c_comment = Comment.new
   end
+  
+  def create
+    @content = Content.find(params[:content_id])
+    
+    @comments = Comment.all
+    comment = current_user.comments.new(comment_params)
+    comment.content_id = @content.id
+    comment.save
+  end
+  
+  def destroy
+    @content = Content.find(params[:content_id])
+    
+    @comments = Comment.all
+    content_comment = @content.comments.find(params[:id])
+    content_comment.destroy
+  end
+  
+  
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root :to => "contents#index"
-    
-    
+
+
     resource :homes do
      get :about, on: :collection
     end
-    
+
 
     resources :users, only: [:index, :show, :edit, :update] do
      resources :relationships, only: [:index]
      resource  :relationships, only: [:create, :destroy]
     end
-    
+
 
     resources :contents do
      resources :favorites, only: [:index]
@@ -22,11 +22,11 @@ Rails.application.routes.draw do
      resources :comments,  only: [:index]
      resource  :comments,  only: [:create, :destroy]
     end
-    
-    
+
+
     get 'chat/:id' => 'chats#show', as: 'chat'
- 
+
     resources :chats, only: [:index, :create]
-  
-  
+
+
 end
