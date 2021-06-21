df995af20d4a269e13d3fd59684215de8c4d4473
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-21 12:15:37 +0000

    [fix layoute/users(edit) devise(sign_up sign_in)]

--- app/assets/javascripts/preview_img.js
+++
@@ -1,7 +0,0 @@
-$('#user_profile_image').on('change', function (e) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-          $("#preview_image").attr('src', e.target.result);
-      }
-      reader.readAsDataURL(e.target.files[0]);
-  });
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   has_many :chats
   has_many :user_rooms
   has_many :rooms, through: :user_rooms
-  
+
   # 自分がフォローしているユーザーとの関連
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
   has_many :followings, through: :active_relationships, source: :follower
@@ -25,24 +25,24 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :following
 
-  
+
 # helper method------------------------------
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
   end
- 
-  
+
+
 # validates----------------------------------
   validates :name,
     presence: true,
     uniqueness: true,
     length: { in: 2..20 }
-  
+
   validates :introduction,
-    length: { maximum: 50 }
+    length: { maximum: 300 }
 
   validates :greeting,
-    length: { maximum: 15 }
+    length: { maximum: 20 }
 
 
 end
