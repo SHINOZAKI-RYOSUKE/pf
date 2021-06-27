bc12738bc8943c30487627b6d4e28b8e0bb3da16
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-27 02:45:56 +0000

    [add notification/model,controller,view,routes]

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,10 @@ class Comment < ApplicationRecord
 # relation-----------------------------------
   belongs_to :user
   belongs_to :content
-  
-  
+  has_many :notifications, dependent: :destroy
+
 # validates----------------------------------
-  validates :comment, 
+  validates :comment,
     presence: true,
     length: { in: 1..1000 }
 
