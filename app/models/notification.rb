a34ca2dd619b729298480028956a4db5d9b9220a
“SHINOZAKI-RYOSUKE” <gibson335m@gmail.com>
2021-06-27 14:19:43 +0000

    [test]

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,5 +7,8 @@ class Notification < ApplicationRecord
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
   
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
   
 end
